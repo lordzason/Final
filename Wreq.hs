7dcbed1c4ef6101301d5b73ab381c0ddb8f9f85d
lordzason <chenzhi17@grinnell.edu>
2016-12-01 13:12:04 -0600

    Successfully converts poetry into syllables!

--- Wreq.hs
+++ Wreq.hs
@@ -4,6 +4,7 @@ module Project where
 
 -- Referenced: http://codereview.stackexchange.com/questions/115066/using-wreq-and-lens-libraries-to-query-prosper-for-account-info
 --- http://stackoverflow.com/questions/38641568/extracting-a-list-of-values-from-a-list-of-maybes-without-fromjust
+
 -- X-Mashape-Key, 4XcFUg43ADmsh4L7DL6oOK6tGNErp1jzZeRjsn7wu8Nj8bkGIe, limited to 2,500 requests per day
 
 import Control.Exception as E
@@ -13,6 +14,7 @@ import Data.Aeson
 import Data.Aeson.Lens (key)
 import Data.Aeson.Lens (_String, key)
 import Data.Aeson (Value)
+import Data.ByteString.Char8
 import Data.Char
 import Data.List.Split
 import Data.Map as Map
@@ -23,6 +25,7 @@ import Data.Text (Text)
 import Data.Text.Strict.Lens
 import Network.HTTP.Client
 import Network.Wreq
+import Prelude hiding (catch)
 import System.IO
 import System.IO.Error
 import Text.Regex (splitRegex, mkRegex)
@@ -50,27 +53,38 @@ stringToText s = s ^. packed
 parseIntoWords :: String -> [Text]
 parseIntoWords s = [(stringToText u) | u <- (splitRegex (mkRegex "[^a-zA-Z+]") s), u /= ""]
 
--- 
-extractNums :: [Value] -> [Double]
-extractNums s = [(toRealFloat x) | (Number x) <- s]
-
--- Gets the syllable for the input from Words API
+-- Makes a GET request to the Words API for a specific word
 getSyllables :: Text -> IO (Maybe Value)
 getSyllables input = do
-  let opts = defaults & header "Accept" .~ ["application/json"] & header "X-Mashape-Key" .~ ["4XcFUg43ADmsh4L7DL6oOK6tGNErp1jzZeRjsn7wu8Nj8bkGIe"]
-  resp <- ((getWith opts (createURL (input <> "/syllables"))) >>= asJSON) :: IO (Response Value)
-  return(resp ^? Network.Wreq.responseBody . key "syllables" . key "count")
+    let opts = defaults & header "Accept" .~ ["application/json"] & header "X-Mashape-Key" .~ ["4XcFUg43ADmsh4L7DL6oOK6tGNErp1jzZeRjsn7wu8Nj8bkGIe"]
+    resp <- (getWith opts (createURL (input <> "/syllables"))) `E.catch` handler
+    let remaining = (resp ^. Network.Wreq.responseHeader "X-RateLimit-requests-Remaining")
+    System.IO.putStr "Free requests remaining: "
+    Data.ByteString.Char8.putStrLn remaining
+    return (resp ^? Network.Wreq.responseBody . key "syllables" . key "count")
+    where
+      handler e@(StatusCodeException s _ _)
+          | s ^. statusCode == 404 = ioError (userError ((textToString input) ++ " is not defined in Words API."))
 
-main = toTry `catchIOError` handler
+-- Gets the syllables from a list of maybe values
+extractSyllables :: [(Maybe Value)] -> [Double]
+extractSyllables r = [(toRealFloat x) | (Number x) <- (Prelude.concatMap customMaybeToList r)]
+    where
+      customMaybeToList :: Maybe Value -> [Value]
+      customMaybeToList Nothing = [(Number 1)]
+      customMaybeToList (Just x) = [x]
 
-toTry :: IO ()
-toTry = forever $ do
-    putStr "Enter a line of poetry: "
-    input <- getLine
-    print (parseIntoWords input)
-    result <- sequence (Prelude.map getSyllables (parseIntoWords input))
-    print (extractNums (Prelude.concatMap maybeToList result))
+main :: IO ()
+main = do
+    System.IO.putStr "Please enter a line of poetry or enter 'Fin' to finish: "
+    input <- System.IO.getLine
+    if ("Fin" == input)
+        then return ()
+        else do
+            print (parseIntoWords input)
+            result <- sequence (Prelude.map (\i -> (getSyllables i) `E.catch` handler) (parseIntoWords input))
+            print (extractSyllables result)
+            main
 
-
-handler :: IOError -> IO ()
-handler e = putStrLn "Error processing input. Please enter another line."
+handler :: IOError -> IO (Maybe Value)
+handler _ = return Nothing
